5ce20a312ed99ccbcb440b6ebd8f039c2cdca09b
Tamil Prabha <prabha.58.ece@gmail.com>
2022-07-06 11:52:54 +0530

    add port

--- app.js
+++ app.js
@@ -29,6 +29,6 @@ app.get("/video", function (req, res) {
 });
  
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started listening on port 3000!");
 });
